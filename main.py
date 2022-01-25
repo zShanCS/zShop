09c4a7e5ed3c51132f7745bd6b93c02282770d43
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2022-01-25 20:05:38 +0500

    disallowed duplication of email in db ❌

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, HTTPException, status
+from fastapi.params import Depends
+from sqlalchemy.orm.session import Session
+
+from src import crud, schemas, models
+from src.database import engine, get_db
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
-@app.get('/products')
-def all_products():
-    return 'these are all the products'
+
+@app.get('/items/', tags=['Item'], response_model=List[schemas.Item])
+def all_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_items(skip=skip, limit=limit, db=db)
+
+
+@app.get('/item/{id}', tags=['Item'], response_model=schemas.Item)
+def get_item_by_id(id: int, db: Session = Depends(get_db)):
+    item = crud.get_item(db=db, item_id=id)
+    if item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No Item with id: {id} found.')
+    return item
+
+
+@app.post("/users/", tags=['User'],  response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user_email=user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/{user_id}", tags=['User'], response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return db_user
+
+
+@app.get("/users/", tags=['User'], response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.post('/user/{user_id}/items', tags=['User'], response_model=schemas.Item)
+def create_user_item(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    user = crud.get_user(db=db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'no user with {user_id} found')
+    return crud.create_user_item(db=db, item=item, owner_id=user.id)
--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, index=True)
-    price = Column(Float, index=True)
+    price = Column(Float, index=True, nullable=False)
     description = Column(String)
 
     owner_id = Column(Integer, ForeignKey('users.id'))
@@ -20,7 +20,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    email = Column(String, index=True)
-    hashed_password = Column(String)
+    email = Column(String, index=True, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
 
     items = relationship('Item', back_populates='owner')
